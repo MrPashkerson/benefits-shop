83874708ff3ae231101db18f4351c514bb015892
Pavel <pavelromanov1226@gmail.com>
2024-04-27 18:14:21 +0300

    feat: add HeaderComponent with new navbar

---
+++ src/app/constants/index.ts
@@ -0,0 +1,24 @@
+export const profileNavItems = [
+  {
+    title: 'Личная информация',
+    url: '/account',
+    icon: '/assets/icons/user.svg',
+  },
+  {
+    title: 'Мои покупки',
+    url: '/account/purchases',
+    icon: '/assets/icons/purchases.svg',
+  },
+  {
+    title: 'Мои заказы',
+    url: '/account/orders',
+    icon: '/assets/icons/orders.svg',
+  },
+  {
+    title: 'Выйти',
+    url: '/logout',
+    icon: '/assets/icons/logout.svg',
+  },
+]
+
+export const noHeaderFooterUrls = ['/create-account', '/login', '/recover-password']
