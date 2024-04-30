abc6c2ee542a8e1cf8d9714700c1aa8edc4eaa9d
Pavel <pavelromanov1226@gmail.com>
2024-04-30 20:08:17 +0300

    feat: rework product page

--- src/app/(pages)/products/[slug]/page.tsx
+++ src/app/(pages)/products/[slug]/page.tsx
@@ -34,46 +34,25 @@ export default async function Product({ params: { slug } }) {
     notFound()
   }
 
-  const { layout, relatedProducts } = product
+  const { relatedProducts } = product
 
   return (
-    <React.Fragment>
+    <>
       <ProductHero product={product} />
-      <Blocks blocks={layout} />
       {product?.enablePaywall && <PaywallBlocks productSlug={slug as string} disableTopPadding />}
       <Blocks
         disableTopPadding
         blocks={[
           {
             blockType: 'relatedProducts',
-            blockName: 'Related Product',
+            blockName: 'Вас может заинтересовать',
             relationTo: 'products',
             introContent: [
               {
-                type: 'h4',
+                type: 'h3',
                 children: [
                   {
-                    text: 'Related Products',
-                  },
-                ],
-              },
-              {
-                type: 'p',
-                children: [
-                  {
-                    text: 'The products displayed here are individually selected for this page. Admins can select any number of related products to display here and the layout will adjust accordingly. Alternatively, you could swap this out for the "Archive" block to automatically populate products by category complete with pagination. To manage related posts, ',
-                  },
-                  {
-                    type: 'link',
-                    url: `/admin/collections/products/${product.id}`,
-                    children: [
-                      {
-                        text: 'navigate to the admin dashboard',
-                      },
-                    ],
-                  },
-                  {
-                    text: '.',
+                    text: 'Вас может заинтересовать',
                   },
                 ],
               },
@@ -82,7 +61,7 @@ export default async function Product({ params: { slug } }) {
           },
         ]}
       />
-    </React.Fragment>
+    </>
   )
 }
 
